06cd03e0fe1b4715d6a5aeb98113c79d5dedeb6b
Nishant Sapkota <snishant306@gmail.com>
2023-10-27 20:11:31 +0545

    chore: fix prettier formatting

--- src/components/app/navbar.tsx
+++ src/components/app/navbar.tsx
@@ -7,10 +7,10 @@ export default function AppNavbar() {
   return (
     <nav className="flex justify-between px-2 py-7 md:px-10">
       <Link to="/" className="font-sans text-xl uppercase">
-      <div className="flex items-center">
-        <img src="/knowNepalLogo.png" alt="" className="w-8 h-8 mr-2" />
-        Know Nepal
-      </div>
+        <div className="flex items-center">
+          <img src="/knowNepalLogo.png" alt="" className="mr-2 h-8 w-8" />
+          Know Nepal
+        </div>
       </Link>
 
       <div className="flex gap-x-5">
