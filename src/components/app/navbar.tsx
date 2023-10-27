74d12034055d9d1090c807e2978940cabae77675
Nishant Sapkota <snishant306@gmail.com>
2023-10-27 20:10:03 +0545

    chore: make some ui changes to the website

--- src/components/app/navbar.tsx
+++ src/components/app/navbar.tsx
@@ -7,7 +7,10 @@ export default function AppNavbar() {
   return (
     <nav className="flex justify-between px-2 py-7 md:px-10">
       <Link to="/" className="font-sans text-xl uppercase">
+      <div className="flex items-center">
+        <img src="/knowNepalLogo.png" alt="" className="w-8 h-8 mr-2" />
         Know Nepal
+      </div>
       </Link>
 
       <div className="flex gap-x-5">
